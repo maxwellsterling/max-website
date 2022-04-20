f36418aa237e51fa371f93539aadcdf8a5a1c4fb
maxwellsterling <msterling3@my.bcit.ca>
2022-04-19 19:10:26 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Pdf from "./documents/Max_Resume_2022.pdf"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h3 className="btn-shell">
+          <a className="btn" href={Pdf} target="_blank">MS</a>
+        </h3>
+        <h3 className="no-touch">|</h3>
+        <h3 className="btn-shell" >
+          <a className="btn">Contact</a>
+        </h3>
+      </div>
+      <div className="App-body">
+        <div className="no-touch software-developer-center" >
+          <b>
+            <span className="software-developer-left">Software </span>
+            <span className="software-developer-right">Developer</span>
+          </b><br />
+          <a className="btn"></a>
+          <a className="btn"></a>
+        </div>
+        <h3 className="no-touch projects">
+          Projects currently in development
+        </h3>
+      </div>
+      <div className="footer">
+        <p>© 2022 Maxwell Sterling</p>
+      </div>
     </div>
   );
 }
