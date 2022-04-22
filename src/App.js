59765a72fbfc3473f0cca931788016c6225cc51b
maxwellsterling <msterling3@my.bcit.ca>
2022-04-22 14:59:20 -0700

    Added icons and links

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import "./App.css";
-import Pdf from "./documents/Max_Resume_2022.pdf"
+import Pdf from "./documents/Max_Resume_2022.pdf";
+import "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { GoMarkGithub } from "react-icons/go";
+import { RiLinkedinBoxFill } from "react-icons/ri";
 
 function App() {
+  const iconStyle = {
+    color: "white",
+    fontSize: "25px",
+    margin: "5px",
+
+    "&:hover": {
+      color: "grey"
+    }
+  };
+
   return (
     <div className="App">
       <div className="App-header">
         <h3 className="btn-shell">
-          <a className="btn" href={Pdf} target="_blank">MS</a>
-        </h3>
-        <h3 className="no-touch">|</h3>
-        <h3 className="btn-shell" >
-          <a className="btn">Contact</a>
+          <a className="btn resume" href={Pdf} target="_blank">
+            MS
+          </a>
         </h3>
       </div>
       <div className="App-body">
-        <div className="no-touch software-developer-center" >
+        <div className="no-touch software-developer-center">
           <b>
             <span className="software-developer-left">Software </span>
             <span className="software-developer-right">Developer</span>
-          </b><br />
-          <a className="btn"></a>
-          <a className="btn"></a>
+          </b>
+          <br />
+          <a href="https://github.com/maxwellsterling">
+            <GoMarkGithub
+            onMouseOver={({target})=>target.style.color="grey"}
+            onMouseOut={({target})=>target.style.color="white"}
+            className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/maxwell-sterling-6611b71a1/">
+            <RiLinkedinBoxFill
+            onMouseOver={({target})=>target.style.color="grey"}
+            onMouseOut={({target})=>target.style.color="white"}
+            className="icon" />
+          </a>
         </div>
         <h3 className="no-touch projects">
-          Projects currently in development
+          Projects are currently in development
         </h3>
       </div>
       <div className="footer">
